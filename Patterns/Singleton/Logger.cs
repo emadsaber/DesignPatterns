d963b31f40f8fd945443eb68358e98992fb0febf
emadsaber <omda.sharkawy@gmail.com>
2018-06-07 12:04:39 +0200

    First check in - Singleton

---
+++ Patterns/Singleton/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Patterns.Singleton
+{
+    public class Logger
+    {
+        private static Logger _instance;
+        public static Logger Instance { get { return _instance != null ? _instance : new Logger(); }  }
+
+        private Logger() { }
+
+        public void Log(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using DesignPatterns.Patterns.Singleton;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test_Singleton();
+
+
+            #region Wait
+            Console.ReadKey();
+            #endregion
+        }
+
+        public static void Test_Singleton()
+        {
+            #region Logs
+            Console.WriteLine("---------Singleton tests started---------\n"); 
+            #endregion
+            Logger.Instance.Log("I am a singleton instance");
+            #region Logs
+            Console.WriteLine("\n---------Singleton tests finished---------\n"); 
+            #endregion
+        }
+    }
+}
